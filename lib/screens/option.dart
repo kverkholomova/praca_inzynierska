c1d1345914e99c1b2df2ec1c8e3f13681c67c16d
Verkholomova Karina <verholomova_karina@ukr.net>
2022-08-06 19:59:58 +0200

    Option screen is changed

--- lib/screens/option.dart
+++ lib/screens/option.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:wol_pro_1/screens/wrapper.dart';
 import '../shared/loading.dart';
 
@@ -14,12 +19,22 @@ class OptionChoose extends StatefulWidget {
   State<OptionChoose> createState() => _OptionChooseState();
 }
 
+
 class _OptionChooseState extends State<OptionChoose> {
+
+  bool selected = true;
+  Timer? timer;
 
   late FirebaseMessaging messaging;
   @override
   void initState() {
     super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 20),(timer){
+      setState(() {
+        selected = !selected;
+      });
+    });
     messaging = FirebaseMessaging.instance;
     messaging.getToken().then((value){
       print(value);
@@ -31,22 +46,66 @@ class _OptionChooseState extends State<OptionChoose> {
   Widget build(BuildContext context) {
     return loading ? Loading() : Scaffold(
       body: Container(
-        color: const Color.fromRGBO(234, 191, 213, 0.8),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 250,horizontal: 20),
-          child: Column(
-            children: const [
-              Center(
-                child: StartButton(buttonName: 'Volunteer', optionRef: false,),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 40),
-                child: Center(
-                  child: StartButton(buttonName: 'Refugee', optionRef: true,)
+        color: Color.fromRGBO(237, 233, 253, 8),
+        child: Stack(
+          children: [
+            // Padding(
+            //   padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.45),
+            //   child: Padding(
+            //     padding: const EdgeInsets.symmetric(horizontal: 20),
+            //     child: SizedBox(
+            //         width: double.infinity,
+            //         child: SvgPicture.asset('assets/logo-02.svg')),
+            //   ),
+            // ),
+
+
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.35),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child:
+                  SvgPicture.asset('assets/logo-01.svg'),
+
                 ),
               ),
-            ],
-          ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.35),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Shimmer.fromColors(
+                  baseColor: Colors.black,
+                  highlightColor: Colors.white,
+                  child: Text("Get ready now",
+                  style: GoogleFonts.raleway(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.7),
+              child: Column(
+                children: [
+                  Center(
+                    child: StartButton(buttonName: 'Volunteer', optionRef: false,),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Center(
+                      child: StartButton(buttonName: 'Refugee', optionRef: true,)
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
 
@@ -63,20 +122,29 @@ class StartButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 50,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20)
-      ),
-      child: MaterialButton(
-      color: const Color.fromRGBO(137, 102, 120, 0.8),
-      child: Text(buttonName),
-      onPressed: () async{
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        width: double.infinity,
+        height: 65,
+        decoration: BoxDecoration(
+            color: const Color.fromRGBO(118, 86, 236, 64),
+          borderRadius: BorderRadius.circular(24)
+        ),
+        child: TextButton(
 
-        optionRefugee=optionRef;
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const Wrapper()));
-      },
+        child: Text(buttonName,
+          style: GoogleFonts.raleway(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () async{
+
+          optionRefugee=optionRef;
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const Wrapper()));
+        },
+        ),
       ),
     );
   }
